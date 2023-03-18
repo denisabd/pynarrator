59d04bc4ea65f52be6e1bbf01a68c49a68eb336a
Denis Abdullin <denisabdullincz@gmail.com>
2023-03-18 22:28:20 +0100

    Moved to VS Code

--- src/pynarrator/__init__.py
+++ src/pynarrator/__init__.py
@@ -1,3 +1,4 @@
 from pynarrator.narrate_descriptive import narrate_descriptive
 from pynarrator.pluralize import pluralize
+from pynarrator.chatgpt import gpt_get_completions
 from pynarrator.data import read_data
